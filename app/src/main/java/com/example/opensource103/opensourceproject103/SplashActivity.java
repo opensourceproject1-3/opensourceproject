531f4abd0ec48f9636c26540967ecb6a9019ffc9
unknown <quietgong@naver.com>
2020-06-06 03:27:55 +0900

    Splash 추가

---
+++ app/src/main/java/com/example/opensource103/opensourceproject103/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.opensource103.opensourceproject103;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        ImageView iv = (ImageView)findViewById(R.id.gif_image);
+        Glide.with(this).load(R.raw.intro).into(iv);
+        startLoading();
+    }
+
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 6000); //6초
+    }
+}
