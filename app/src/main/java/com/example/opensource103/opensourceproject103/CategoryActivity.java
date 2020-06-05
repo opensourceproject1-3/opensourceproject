024dad57cf2870849ee4c40890ec3ffb0f9f4e3e
Yanjoo <hanjub3@gmail.com>
2020-06-06 01:54:26 +0900

    카테고리 추가

---
+++ app/src/main/java/com/example/opensource103/opensourceproject103/CategoryActivity.java
@@ -0,0 +1,43 @@
+package com.example.opensource103.opensourceproject103;
+
+import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    int category;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+
+        Intent intent = getIntent();
+        category = intent.getIntExtra("category", 0);
+
+        tabLayout = findViewById(R.id.tabs);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        TabLayout.Tab tab = tabLayout.getTabAt(category);
+        tab.select();
+    }
+}
