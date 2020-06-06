6941e39257d53019781b5a01af5ae15c2592f346
Yanjoo <hanjub3@gmail.com>
2020-06-06 12:40:56 +0900

    시장 사진 추가

--- app/src/main/java/com/example/opensource103/opensourceproject103/MarketFragment.java
+++ app/src/main/java/com/example/opensource103/opensourceproject103/MarketFragment.java
@@ -36,14 +36,14 @@ public class MarketFragment extends Fragment {
 
     private  void setData() {
         adapter.addItem(new CustomDTO(R.drawable.market_logo, "복대가경시장", "복대가경시장입니다."));
-        adapter.addItem(new CustomDTO(R.drawable.mini_logo, "가경터미널시장", "가경터미널시장입니다."));
-        adapter.addItem(new CustomDTO(R.drawable.mini_logo, "육거리종합시장", "육거리종합시장입니다."));
-        adapter.addItem(new CustomDTO(R.drawable.mini_logo, "복대시장", "복대시장입니다."));
-        adapter.addItem(new CustomDTO(R.drawable.mini_logo, "사창시장", "사창시장입니다."));
+        adapter.addItem(new CustomDTO(R.drawable.market_logo2, "가경터미널시장", "가경터미널시장입니다."));
+        adapter.addItem(new CustomDTO(R.drawable.market_logo3, "육거리종합시장", "육거리종합시장입니다."));
+        adapter.addItem(new CustomDTO(R.drawable.market_logo4, "복대시장", "복대시장입니다."));
+        adapter.addItem(new CustomDTO(R.drawable.market_logo5, "사창시장", "사창시장입니다."));
         adapter.addItem(new CustomDTO(R.drawable.mini_logo, "육거리전통시장", "육거리전통시장입니다."));
-        adapter.addItem(new CustomDTO(R.drawable.mini_logo, "운천시장", "운천시장입니다."));
-        adapter.addItem(new CustomDTO(R.drawable.mini_logo, "북부시장", "북부시장입니다."));
-        adapter.addItem(new CustomDTO(R.drawable.mini_logo, "청주평화시장", "청주평화시장입니다."));
-        adapter.addItem(new CustomDTO(R.drawable.mini_logo, "두꺼비시장", "두꺼비시장입니다."));
+        adapter.addItem(new CustomDTO(R.drawable.market_logo6, "운천시장", "운천시장입니다."));
+        adapter.addItem(new CustomDTO(R.drawable.market_logo7, "북부시장", "북부시장입니다."));
+        adapter.addItem(new CustomDTO(R.drawable.market_logo8, "청주평화시장", "청주평화시장입니다."));
+        adapter.addItem(new CustomDTO(R.drawable.market_logo9, "두꺼비시장", "두꺼비시장입니다."));
     }
 }
