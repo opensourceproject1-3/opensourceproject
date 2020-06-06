9b74058220dc4735e29dff87f672eba8e5b294f2
Yanjoo <hanjub3@gmail.com>
2020-06-07 00:11:18 +0900

    상품 DB 연결

---
+++ app/src/main/java/com/example/opensource103/opensourceproject103/ProductActivity.java
@@ -0,0 +1,58 @@
+package com.example.opensource103.opensourceproject103;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ProductActivity extends AppCompatActivity {
+
+    private static final String TAG_ID = "productID";
+    private static final String TAG_NAME = "productName";
+    private static final String TAG_PRICE = "productPrice";
+    private static final String TAG_IMG = "productImage";
+    private static final String TAG_SID = "storeID";
+    private static final String TAG_CID = "categoryID";
+
+    RecyclerView recyclerView;
+    ArrayList<HashMap<String, String>> productList;
+    ProductData data;
+    String storeID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+
+        Intent intent = getIntent();
+        storeID = intent.getStringExtra("storeID");
+
+        data = ProductData.getData();
+        productList = data.getList();
+
+        recyclerView = findViewById(R.id.menu_recyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        ProductAdapter adapter = new ProductAdapter();
+
+        for (int i=0; i<productList.size(); i++) {
+            String sid = productList.get(i).get(TAG_SID);
+            if (storeID.equals(sid)) {
+                String id = productList.get(i).get(TAG_ID);
+                String name = productList.get(i).get(TAG_NAME);
+                int price = Integer.parseInt(productList.get(i).get(TAG_PRICE));
+                String img = productList.get(i).get(TAG_IMG);
+                String category = productList.get(i).get(TAG_CID);
+                adapter.addItem(new ProductModel(id, name, price, img, sid, category));
+            }
+        }
+
+        recyclerView.setAdapter(adapter);
+
+    }
+}
--- app/src/main/java/com/example/opensource103/opensourceproject103/StoreAdapter.java
+++ app/src/main/java/com/example/opensource103/opensourceproject103/StoreAdapter.java
@@ -1,11 +1,13 @@
 package com.example.opensource103.opensourceproject103;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -47,6 +49,7 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.ViewHolder> 
         TextView storeName;
         TextView storeTime;
         TextView storeCategory;
+        RelativeLayout storeContainer;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -54,12 +57,13 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.ViewHolder> 
             storeName = itemView.findViewById(R.id.store_name);
             storeTime = itemView.findViewById(R.id.store_time);
             storeCategory = itemView.findViewById(R.id.store_category);
+            storeContainer = itemView.findViewById(R.id.store_container);
         }
 
-        public void setItem(StoreModel item) {
-            storeImage.setImageResource(R.drawable.mini_logo);
+        public void setItem(final StoreModel item) {
+            storeImage.setImageResource(R.drawable.store);
             storeName.setText(item.getStoreName());
-            storeTime.setText(item.getStoreTime());
+            storeTime.setText("영업시간: " + item.getStoreTime());
             String category = item.getCategoryID();
             if (category.equals("CATEGORY_1")) {
                 storeCategory.setText("축산");
@@ -74,6 +78,15 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.ViewHolder> 
             } else if (category.equals("CATEGORY_6")) {
                 storeCategory.setText("먹거리");
             }
+            storeContainer.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(v.getContext(), ProductActivity.class);
+                    intent.putExtra("storeID", item.getStoreID());
+                    v.getContext().startActivity(intent);
+                }
+            });
+
         }
     }
 }
