49cac3565dd444e9b71c58d6549ed597ef380ac3
Yanjoo <hanjub3@gmail.com>
2020-06-06 20:23:08 +0900

    시장 지도 추가

--- app/src/main/java/com/example/opensource103/opensourceproject103/MarketMap.java
+++ app/src/main/java/com/example/opensource103/opensourceproject103/MarketMap.java
@@ -3,6 +3,7 @@ package com.example.opensource103.opensourceproject103;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MarketMap extends AppCompatActivity {
@@ -13,10 +14,21 @@ public class MarketMap extends AppCompatActivity {
         setContentView(R.layout.activity_market_map);
 
         Intent intent = getIntent();
+        int map = intent.getIntExtra("marketNum", 0);
 
-        TextView textView = findViewById(R.id.map_num);
-        textView.setText(String.valueOf(intent.getIntExtra("marketNum", 0)));
+        ImageView imageView = findViewById(R.id.makret_map);
 
+        int mapRes = 0;
+        switch (map) {
+            case 0:
+                mapRes = R.drawable.map1;
+                break;
+            case 1:
+                mapRes = R.drawable.map2;
+                break;
+        }
+
+        imageView.setImageResource(mapRes);
 
     }
 }
--- app/src/main/java/com/example/opensource103/opensourceproject103/SplashActivity.java
+++ app/src/main/java/com/example/opensource103/opensourceproject103/SplashActivity.java
@@ -26,7 +26,7 @@ public class SplashActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run(){
-                Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
                 startActivity(intent);
                 finish();
             }
