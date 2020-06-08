30bd75aa93e06b31ca752bb12477a985600fa4a2
choisaywhy <sehwa98@icloud.com>
2020-06-08 14:34:07 +0900

    re-linked login, edited password input and readme, added php files

---
+++ DatabaseConnection/Register.php
@@ -0,0 +1,22 @@
+<?php 
+    $con = mysqli_connect("localhost", "sehwa98", "chltpghk33!", "sehwa98");
+    mysqli_query($con,'SET NAMES utf8');
+
+    $userID = $_POST["userID"];
+    $userPassword = $_POST["userPassword"];
+    $userName = $_POST["userName"];
+
+    $statement = mysqli_prepare($con, "INSERT INTO USER VALUES (?,?,?)");
+    mysqli_stmt_bind_param($statement, "sss", $userID, $userPassword, $userName);
+    mysqli_stmt_execute($statement);
+
+
+    $response = array();
+    $response["success"] = true;
+ 
+   
+    echo json_encode($response);
+
+
+
+?>
